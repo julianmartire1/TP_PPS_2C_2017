30123c84a6a658e9449f79aa74544b290c72d343
RodrigoBalabasquer <r.balabasquer@gmail.com>
2017-11-03 22:34:10 -0300

    Modelo de gestor de encuesta

---
+++ Encuesta/pages/encuesta/encuesta.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the EncuestaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-encuesta',
+  templateUrl: 'encuesta.html',
+})
+export class EncuestaPage {
+
+  public encuesta: Array<any> = this.navParams.get("encuesta");;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EncuestaPage');
+  }
+
+}
